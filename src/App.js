8c9e3ec6bd0d2ed4be900bd67c11390bf6240561
jsdnlb <danielb.alzate1@gmail.com>
2021-02-28 18:46:09 -0500

    Uso de props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+/* class HelloWorld extends Component {
+  render() {
+    return <div>Hello world</div>;
+
+  }
+} */
+
+function HelloWorld(props) {
+  return (
+    <div>
+      <h4>{props.ocupation}</h4>
+      <br></br>
+      {props.myText}
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        This is my new Component: <HelloWorld myText="Nombre: Daniel" ocupation="CEO" /> <HelloWorld myText="Nickname: jsdnlb" ocupation="RRHH" />{" "}
       </div>
     );
   }
