5bba237f8ac158b095024cfe4ae58a4a92a9697c
jsdnlb <danielb.alzate1@gmail.com>
2021-02-28 20:46:55 -0500

    Using propTypes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,35 +1,51 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import "./css/Task.css";
-
-const btnDelete = {
-  fontSize: "18px",
-  background: "#ea2027",
-  color: "#FFF",
-  border: "none",
-  padding: "10px 15px",
-  borderRadius: "50%",
-  cursor: "pointer",
-};
 
 class Task extends Component {
 
-    StyleComptleted(){
-        return{
-            fontSize :  "20px",
-            color : this.props.task.done ? "gray": "black",
-            textDecoration: this.props.task.done ? "line-through": "none"
-        }
-    }
+  
+  StyleComptleted() {
+    return {
+      fontSize: "20px",
+      color: this.props.task.done ? "gray" : "black",
+      textDecoration: this.props.task.done ? "line-through" : "none",
+    };
+  }
+  StyleEnabled() {
+    return {
+      fontSize: "18px",
+      background: this.props.task.done ? "gray" : "#ea2027",
+      color: "#FFF",
+      border: "none",
+      padding: "10px 15px",
+      borderRadius: "50%",
+      cursor: "pointer",
+    };
+  }
+
+  changeStyle = () =>{
+    console.log("🚀 ~ file: Task.js ~ line 27 ~ Task ~ this.props.task.done", this.props.task.done)
+  }
+
   render() {
+    Task.propTypes = {
+        task: PropTypes.object.isRequired 
+}
     const { task } = this.props;
     return (
       <div style={this.StyleComptleted()}>
         {task.title} - {task.description}
         <input type="checkbox" />
-        <button style={btnDelete}>X</button>
+        <button onClick={this.changeStyle} style={this.StyleEnabled()}>
+          X
+        </button>
       </div>
     );
   }
+
+ 
+
 }
 
 export default Task;
