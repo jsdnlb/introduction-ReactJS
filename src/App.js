f41b23cf962e4565ca08d222d474ef70c6bd16e8
jsdnlb <danielb.alzate1@gmail.com>
2021-03-14 22:27:55 -0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+// [BrowserRouter as Router] = Sirve para renombrar
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 import tasks from "./sample/tasks.json";
 
 // Components
 import Tasks from "./components/Tasks";
 import TaskForm from "./components/TaskForm";
-import Posts from "./components/Posts"
+import Posts from "./components/Posts";
 
 /* class HelloWorld extends Component {
   state = {
@@ -75,25 +77,40 @@ class App extends Component {
 
   deleteTask = (id) => {
     const newTasks = this.state.tasks.filter((task) => task.id !== id);
-    this.setState({tasks: newTasks})
+    this.setState({ tasks: newTasks });
   };
 
-  checkDone = id => {
-    const newTasks =this.state.tasks.map(task =>{
-      if (task.id === id){
-         task.done = !task.done 
+  checkDone = (id) => {
+    const newTasks = this.state.tasks.map((task) => {
+      if (task.id === id) {
+        task.done = !task.done;
       }
       return task;
-    })
-    this.setState({tasks: newTasks})
-  }
+    });
+    this.setState({ tasks: newTasks });
+  };
 
   render() {
     return (
       <div>
-        <TaskForm addTask={this.addTask} />
-        <Tasks tasks={this.state.tasks} deleteTask={this.deleteTask} checkDone={this.checkDone} />
-        <Posts/>
+        <Router>
+          <Link to="/">Home</Link>
+          <br />
+          <Link to="/posts">Posts</Link>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return (
+                <div>
+                  <TaskForm addTask={this.addTask} />
+                  <Tasks tasks={this.state.tasks} deleteTask={this.deleteTask} checkDone={this.checkDone} />
+                </div>
+              );
+            }}
+          ></Route>
+          <Route path="/posts" component={Posts} />
+        </Router>
       </div>
     );
   }
